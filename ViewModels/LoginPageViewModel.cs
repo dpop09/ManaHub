8e6a6f9fa4fef49b6e44bde9f4e491048ac7af9c
Dan Pop <danpop32359@gmail.com>
2026-01-11 13:39:40 -0500

    Incorportated a basic MVVM archiecture for page navigation

---
+++ ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ManaHub.MVVMs;
+using System.Windows.Input;
+
+namespace ManaHub.ViewModels
+{
+    internal class LoginPageViewModel : ViewModelBase
+    {
+        private MainWindowViewModel _mainVM;
+        public ICommand GoToCreateAccountPageCommand { get; }
+
+        // We pass the MainViewModel through the constructor
+        public LoginPageViewModel(MainWindowViewModel mainVM)
+        {
+            _mainVM = mainVM;
+            GoToCreateAccountPageCommand = new RelayCommand(o => GoToCreateAccountPage());
+        }
+
+        private void GoToCreateAccountPage()
+        {
+            _mainVM.CurrentView = new CreateAccountPageViewModel(this._mainVM);
+        }
+    }
+}
