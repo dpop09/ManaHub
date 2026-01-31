e23d48e3428698cf98b033477f75bacb3751c3d6
Dan Pop <danpop32359@gmail.com>
2026-01-30 22:53:55 -0500

    added a card display example

---
+++ Views/CardDisplay.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ManaHub.Views
+{
+    /// <summary>
+    /// Interaction logic for CardDisplay.xaml
+    /// </summary>
+    public partial class CardDisplay : UserControl
+    {
+        public CardDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}
